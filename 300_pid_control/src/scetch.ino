5f84421ab843f641d33dd75416c1686b6066e2b0
Hans Erik Fjeld <hans.erik.fjeld@embida.no>
2019-09-15 17:57:12 +0200

    Adapted project to use Arduino PID library from br3ttb

--- 300_pid_control/src/scetch.ino
+++ 300_pid_control/src/scetch.ino
@@ -1,6 +1,7 @@
 #include "RTClib.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <PID_v1.h>
 #define ONE_WIRE_BUS 7
 
 //-----------------------------------------------------------
@@ -11,22 +12,28 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors (&oneWire);
 int numberOfDevices;
 DeviceAddress tempDeviceAddress;
+int indoor_temp_sensor = 0, outdoor_temp_sensor = 1;
+float sensor_indoor_temp,sensor_outdoor_temp;
 //RTC
 RTC_PCF8523 rtc;
+DateTime now;
 char daysOfTheWeek[7][12] = {"Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-//Global vars
+//GPIO
 const int LED_RB = 4;
 const int LEDRB_PWM = 5;
 const int LED_COB = 2;
 const int LEDCOB_PWM = 3;
 int loopCntr = 0;
 int pwm_adjust = 0;
-DateTime now, prev_time;
-float current_indoor_temp,current_outdoor_temp;
-float prev_indoor_temp, prev_outdoor_temp;
-float temp_error,accumulated_error,rate_error,prev_error;
-float Ki,Kp,Kd;
-float pid_output;
+//PID
+double pid_input,pid_output,pid_setpoint;
+//Aggressive regulation when far away from setpoint.
+double pid_aggKi = 4, pid_aggKp = 0.2, pid_aggKd = 1;
+//Presice regulation near setpoint.
+double pid_consKi = 1, pid_consKp = 0.05, pid_consKd = 0.25;
+PID myPID(&pid_input, &pid_output, &pid_setpoint,pid_consKp,pid_consKi,pid_consKd,DIRECT);
+
+
 extern String getFullTimeString(DateTime aTime);
 extern String getClockString( DateTime aTime);
 extern int isFutureevent(DateTime now);
@@ -45,87 +52,103 @@ void setup () {
   pinMode(LEDCOB_PWM, OUTPUT);
   pwm_set(LEDRB_PWM,0);
   pwm_set(LEDCOB_PWM,0);
-  now = rtc.now();
-  prev_time = now; //If now is last time, it is the first time.. Right?
   while (!Serial) {
     delay(1);  // for Leonardo/Micro/Zero
   }
-
   Serial.begin(115200);
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
   }
-
   if (! rtc.initialized()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
     //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2019, 7, 29, 22, 01, 0));
   }
+  now = rtc.now();
+  // Start OneWire sensor
   sensors.begin();
   numberOfDevices = sensors.getDeviceCount();
-  Serial.print("Locating devices...");
-  Serial.print("Found ");
+  Serial.print("Locating devices... Found: ");
   Serial.print(numberOfDevices, DEC);
   Serial.println(" devices.");
   printDallasDevices();
-  current_indoor_temp = 0;
-  current_outdoor_temp = 0;
-  prev_indoor_temp = 0;
-  prev_outdoor_temp =0;
-  temp_error = 0;
-  accumulated_error = 0;
-  prev_error = 0;
-  Kp = 2;
-  Ki = 5;
-  Kd = 1;
+  sensors.getAddress(tempDeviceAddress, indoor_temp_sensor);
+  sensor_indoor_temp = sensors.getTempC(tempDeviceAddress);
+  pid_setpoint = 22.0; //Deg centigradess
+  pid_input = double(sensor_indoor_temp);
+  myPID.SetMode(AUTOMATIC);
+  
 }
 
 void loop () {
+    
     loopCntr++;
-    float setpoint = 24.0; //Ideal temperature is 24C
-    float tolerance = 3.0; // Tolerance is
-    float setpoint_max_limit = setpoint + tolerance;
-    float setpoint_min_limit = setpoint - tolerance;
     now = rtc.now(); 
-    int elapsed_time = now.unixtime- prev_time.unixtime;
     String myTD = getFullTimeString(now);
-    Serial.print("Run:"+ String(loopCntr));
-    Serial.println();
-    Serial.print(myTD);
-    Serial.println();
-    for(int i=0;i<numberOfDevices; i++) {
-    int indoor_temp_sensor = 0;
-    int outdoor_temp_sensor = 1;
+    String Serial_Message;
+    Serial_Message = "Run: "+ String(loopCntr) + '\n';
+    Serial_Message +=( myTD + '\n');
+    /*
+      Get sensor temperatures
+    */
     sensors.getAddress(tempDeviceAddress, indoor_temp_sensor);
-    current_indoor_temp = sensors.getTempC(tempDeviceAddress);
+    sensor_indoor_temp = sensors.getTempC(tempDeviceAddress);
     sensors.getAddress(tempDeviceAddress, outdoor_temp_sensor);
-    current_outdoor_temp = sensors.getTempC(tempDeviceAddress);
-    Serial.print("T1: ");
-    Serial.print(current_indoor_temp);
-    Serial.print("T2: ");
-    Serial.print(current_outdoor_temp);
-    temp_error = setpoint-current_indoor_temp;
-    accumulated_error+= temp_error * elapsed_time;
-    rate_error = (temp_error-prev_error)/elapsed_time;
-    pid_output = Kp * temp_error + Ki * accumulated_error + Kd * rate_error;
+    sensor_outdoor_temp = sensors.getTempC(tempDeviceAddress);
+    Serial_Message += ('T_indoor = ' + String(sensor_indoor_temp) + '\n');
+    Serial_Message += ('T_outdoor = ' + String(sensor_outdoor_temp) + '\n');
+    /*
+      PID Algoritm
+    */
+    pid_input = double(sensor_indoor_temp);
+    double gap = abs(pid_setpoint-pid_input); //distance away from setpoint
+    bool pid_mode_aggressive;
+    if (gap < 2.0)
+    {//we're close to setpoint, use conservative tuning parameters
+      myPID.SetTunings(pid_consKp, pid_consKi, pid_consKd);
+      pid_mode_aggressive = false;
+    }
+    else
+    {
+      //we're far from setpoint, use aggressive tuning parameters
+      myPID.SetTunings(pid_aggKp, pid_aggKi, pid_aggKd);
+      pid_mode_aggressive = true;
+    }
+    myPID.Compute();
+    /*
+      PID MESSAGE
+    */
+    Serial_Message +=('T_err = ' + String(gap) + '\n');
+    Serial_Message +=('PID_output = ' + String(pid_output) + '\n');
+    if(pid_mode_aggressive){
+      Serial_Message +=('PID_mode = AGGRESSIVE'+ '\n');
+      Serial_Message +=('Kd = ' + String(pid_aggKd) + ' , ' );
+      Serial_Message +=('Ki = ' + String(pid_aggKi) + ' , ' );
+      Serial_Message +=('Kp = ' + String(pid_aggKp) + '\n' ); 
+    }
+    else
+    {
+      Serial_Message +=('PID_mode = CONSERVATICE'+ '\n');
+      Serial_Message +=('Kd = ' + String(pid_consKd) + ' , ' );
+      Serial_Message +=('Ki = ' + String(pid_consKi) + ' , ' );
+      Serial_Message +=('Kp = ' + String(pid_consKp) + '\n' ); 
+    }  
+        
     digitalWrite(LED_RB,LOW);//Active low?
-    digitalWrite(LED_COB,HIGH);//YES
-    pwm_set(LEDRB_PWM,90);
-    pwm_set(LEDCOB_PWM,0);
-    Serial.print("Purple lights ON, 35%, White lights ON, 0%");
-    //Store the timestamp of this run for the next run
-    prev_time = now;
-    prev_error = temp_error;
-
-    delay(10000);
+    digitalWrite(LED_COB,LOW);//YES
+    pwm_set(LEDRB_PWM,140);
+    pwm_set(LEDCOB_PWM,140);
+    Serial_Message += ("Purple lights ON, 60%, White lights ON, 60%"+'\n');
+    
+    if(loopCntr%10){
+      Serial.print(Serial_Message);
+    }
     loopCntr++;
     if(loopCntr >= 10000){
         loopCntr = 0;
     }
+    delay(1000);
 }
 
 String getFullTimeString(DateTime aTime){
